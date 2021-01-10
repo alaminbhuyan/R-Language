13aa060321630453427ba6121a21ba66904a279d
alamin bhuyan <alaminbhuyan321@gmail.com>
2021-01-10 20:37:18 +0600

    Create First.ipynb file

---
+++ variable.R
@@ -0,0 +1,12 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: alami
+# Created on: 11/10/2020
+
+a = 4
+b = 5
+print(a+b)
+assign("var","alamin")
+print(var)
+print(class(a))
+print(class(var))
